af09b9a1eeb4fe8dc0370d9a8f12a72265ac51af
Ousman Touray <otouray14@gmail.com>
2023-02-28 11:10:17 -0500

    tweaking logic

--- index.js
+++ index.js
@@ -4,11 +4,37 @@ const score = document.getElementById('score');
 const jeopardyCats = [
     {
         genre: "History",
-        questions: []
+        questions: [
+            {
+                question: "Where was Ghengis Khan Born",
+                answer: document.createElement('form')
+            },
+            {
+                question:"How old was Alexander the Great when he passed?",
+                answer: document.createElement('form')
+            },
+            {
+                question:"What was the name of Mao Zedong's plan to industrialize China?",
+                answer: document.createElement('form')
+            }
+        ]
     },
     {
         genre:"Programming",
-        questions: []
+        questions: [
+            {
+                question: "What is the name of the person who invented the language JavaScript?",
+                answer: document.createElement('form')
+            },
+            {
+                question: "What is the difference between innerText and innerHTML?",
+                answer: document.createElement('form')
+            },
+            {
+                question: "lorem",
+                answer: document.createElement('form')
+            }
+        ]
     },
     {
         genre:"Culture",
@@ -19,3 +45,4 @@ const jeopardyCats = [
         questions:[]
     }
 ]
+
