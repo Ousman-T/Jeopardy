316129f781a639f8ff277f636d2df432fbb1c7f8
Ousman Touray <otouray14@gmail.com>
2023-02-28 09:50:15 -0500

    creating logic

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// shuffling
+function shuffle(a){}
+
+
+class GameShow {
+    constructor(element, options={}){
+        this.useCategoryIds = ["Programming", "History", "Science", "Math", "Sports"];
+    }
+}
+
+const game = new GameShow (document.querySelector(".app"), {});
